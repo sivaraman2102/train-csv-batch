665e32d0a6998c97d910ea59102cead0f0e4a6cb
sivaraman2102 <73744953+sivaraman2102@users.noreply.github.com>
2021-12-12 21:18:24 +0530

    Modified Job completion listener

--- src/main/java/com/siva/traincsvbatch/listener/JobCompletionListener.java
+++ src/main/java/com/siva/traincsvbatch/listener/JobCompletionListener.java
@@ -4,10 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobExecutionListener;
 import org.springframework.batch.core.listener.JobExecutionListenerSupport;
 import org.springframework.stereotype.Component;
 @ Component
-public class JobCompletionListener extends JobExecutionListenerSupport {
+public class JobCompletionListener implements JobExecutionListener {
 
     private static final Logger log =
             LoggerFactory.getLogger(JobCompletionListener.class);
@@ -19,4 +20,11 @@ public class JobCompletionListener extends JobExecutionListenerSupport {
 
         }
     }
+
+	@Override
+	public void beforeJob(JobExecution jobExecution) {
+		// TODO Auto-generated method stub
+		log.info("BATCH JOB STARTED WITH {} ID AND STARTED AT {}",jobExecution.getJobId(),jobExecution.getStartTime());
+	}
+   
 }
